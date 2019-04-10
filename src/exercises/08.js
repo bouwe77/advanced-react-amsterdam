98328f6293ca12ae9df40ab0bccf164b3d557041
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-04-10 15:16:45 +0200

    .

--- src/exercises/08.js
+++ src/exercises/08.js
@@ -3,35 +3,15 @@
 import React from 'react'
 import {Switch} from '../switch'
 
-// 🦉 Sometimes, just being made aware of the changes that occur internally
-// isn't enough and what you really want is the ability to completely control
-// how state updates are being made.
-//
-// In this example, we want to prevent the toggle from updating the toggle state
-// after it's been clicked 4 times in a row before resetting.
-//
-// We could definitely add an option to our component and add that logic in our
-// reducer, but there's a neverending list of logical customizations that people
-// could want out of our custom hook and we don't want to have to code for every
-// one of those cases.
-//
-// So instead we're going to apply a computer science pattern called
-// "Inversion of Control" where we effectively say: "Here you go! You have
-// complete control over how this thing works. It's now your responsibility."
-//
-// As an asside, before React Hooks were a thing, this was pretty tricky to
-// implement and resulted in pretty weird code, but with useReducer, this is WAY
-// better. I ❤️ hooks. 😍
-
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
 const noop = () => {}
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case useToggle.types.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case useToggle.types.reset: {
       return initialState
     }
     default: {
@@ -39,6 +19,8 @@ function toggleReducer(state, {type, initialState}) {
     }
   }
 }
+
+function useReducerWithValidation(reducer) {}
 
 function useToggle({
   onToggle = noop,
@@ -51,12 +33,12 @@ function useToggle({
 
   function toggle() {
     const newOn = !on
-    dispatch({type: 'toggle'})
+    dispatch({type: useToggle.types.toggle})
     onToggle(newOn)
   }
 
   function reset() {
-    dispatch({type: 'reset', initialState})
+    dispatch({type: useToggle.types.reset, initialState})
     onReset(initialState.on)
   }
 
@@ -76,21 +58,12 @@ function useToggle({
   }
 }
 
-// 💯 Our `toggleReducer` is pretty simple, so it's not a huge pain for people
-// to implement their own. However, in a more realistic scenario, people may
-// struggle with having to basically re-implement our entire reducer which could
-// be pretty complex.
-// So see if you can provide a nice way for people to be able to use the
-// `toggleReducer` themselves if they so choose. Feel free to test this out by
-// changing the Usage example below (even though I say don't make changes) to
-// use the default reducer instead of having to re-implement what to do when
-// the action type is 'reset'
+useToggle.reducer = toggleReducer
 
-// 💯 Requiring people to know what action types are available and code them
-// is just asking for bug-inducing typos. See if you can figure out a good way
-// to help people avoid typos in those strings by perhaps putting all possible
-// action types on an object somewhere and referencing them instead of hard
-// coding them.
+useToggle.types = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
 
 // 💯 Here's a fun one... Something that would be bad news is if folks start
 // using our reducer to store state in our reducer that the toggle shouldn't
@@ -110,22 +83,10 @@ function Usage() {
   const [timesClicked, setTimesClicked] = React.useState(0)
 
   function toggleStateReducer(state, action) {
-    // 💯 I, Hannah Hundred, give you permission to edit this function for
-    // the extra credit outlined above. 😘
-    switch (action.type) {
-      case 'toggle': {
-        if (timesClicked >= 4) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === useToggle.types.toggle && timesClicked >= 4) {
+      return {on: state.on, foo: 'bar'}
     }
+    return useToggle.reducer(state, action)
   }
 
   const {on, getTogglerProps, reset} = useToggle({
